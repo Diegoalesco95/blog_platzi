454cfa6f1f2d26e5cfde7ad3f211fb3dc936130c
Diego Alejandro Osorio <42728227+Diegoalesco95@users.noreply.github.com>
2020-03-03 16:30:13 -0500

    Redux Thunk

--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { connect } from 'react-redux';
 import * as usuariosActions from '../../actions/usuariosActions';
 
 class Usuarios extends Component {
   componentDidMount() {
-    // const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-    // this.setState({
-    //   usuarios: response.data
-    // });
     this.props.traerTodos();
   }
 
@@ -22,8 +17,6 @@ class Usuarios extends Component {
     ));
 
   render() {
-    console.log(this.props);
-
     return (
       <div>
         <table className="tabla table table-striped table-bordered">
