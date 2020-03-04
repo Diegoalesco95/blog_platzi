efdd5b034f8e42fd7d98e4ba7aa04be022569001
Diego Alejandro Osorio <42728227+Diegoalesco95@users.noreply.github.com>
2020-03-04 16:25:44 -0500

    Inmutabilidad

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import { TRAER_POR_USUARIO, LOADING, ERROR } from '../types/publicacionesTypes';
+import * as usuariosTypes from '../types/usuariosTypes';
+
+const { TRAER_TODOS: USUARIOS_TRAER_TODOS } = usuariosTypes;
 
 export const traerPorUsuario = key => async (dispatch, getState) => {
   const { usuarios } = getState().usuariosReducer;
@@ -9,6 +12,18 @@ export const traerPorUsuario = key => async (dispatch, getState) => {
   const response = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${usuario_id}`);
 
   const publicaciones_actualizadas = [...publicaciones, response.data];
+
+  const publicaciones_key = publicaciones_actualizadas.length - 1;
+  const usuarios_actualizados = [...usuarios];
+  usuarios_actualizados[key] = {
+    ...usuarios[key],
+    publicaciones_key
+  };
+
+  dispatch({
+    type: USUARIOS_TRAER_TODOS,
+    payload: usuarios_actualizados
+  });
 
   dispatch({
     type: TRAER_POR_USUARIO,
