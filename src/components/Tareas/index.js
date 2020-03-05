ddb4486270ebbc819318a7b931d8db1e37e5b8ad
Diego Alejandro Osorio <42728227+Diegoalesco95@users.noreply.github.com>
2020-03-05 13:44:08 -0500

    Mapear Objetos

--- src/components/Tareas/index.js
+++ src/components/Tareas/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import PageLoading from '../General/PageLoading';
+import Fatal from '../General/Fatal';
 import * as tareasActions from '../../actions/tareasActions';
 
 class Tareas extends Component {
@@ -8,10 +9,41 @@ class Tareas extends Component {
     this.props.traerTodas();
   }
 
+  mostrarContenido = () => {
+    const { tareas, loading, error } = this.props;
+
+    if (loading) {
+      return <PageLoading />;
+    }
+    if (error) {
+      return <Fatal mensaje={error} />;
+    }
+    return Object.keys(tareas).map(usu_id => (
+      <div key={usu_id}>
+        <h2>Usuario {usu_id}</h2>
+        <div className="contenedor_tareas">{this.ponerTareas(usu_id)}</div>
+      </div>
+    ));
+  };
+
+  ponerTareas = usu_id => {
+    const { tareas } = this.props;
+    const por_usuario = {
+      ...tareas[usu_id]
+    };
+
+    return Object.keys(por_usuario).map(tar_id => (
+      <div key={tar_id}>
+        <input type="checkbox" defaultChecked={por_usuario[tar_id].completed} className="mr-3" />
+        {por_usuario[tar_id].title}
+      </div>
+    ));
+  };
+
   render() {
     console.log(this.props);
 
-    return <div>Tareas Saludar</div>;
+    return <div>{this.mostrarContenido()}</div>;
   }
 }
 
